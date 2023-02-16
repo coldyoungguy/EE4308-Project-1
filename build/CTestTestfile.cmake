1e9f06131f11134ed0824c6a5d58094bea59dd51
coldyoungguy <koh.yangkai@gmail.com>
2023-02-16 13:07:09 -0800

    update ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yangkai/ros_code/EE4308/proj1/src
-# Build directory: /home/yangkai/ros_code/EE4308/proj1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ee4308_turtle")
-subdirs("ee4308_bringup")
--- build/ee4308_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yangkai/ros_code/EE4308/proj1/src/ee4308_bringup
-# Build directory: /home/yangkai/ros_code/EE4308/proj1/build/ee4308_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
