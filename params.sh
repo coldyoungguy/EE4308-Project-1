2ac70e2f866557eff76df8beaf060e414cb85d98
coldyoungguy <koh.yangkai@gmail.com>
2023-02-21 22:45:25 -0800

    stuff

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export WORLD="world23proj1"
-# export EE4308_TASK="proj1"
+export WORLD="world23proj1"
+export EE4308_TASK="proj1"
 
-export WORLD="world21"
-export EE4308_TASK="labs"
+# export WORLD="world21"
+# export EE4308_TASK="labs"
